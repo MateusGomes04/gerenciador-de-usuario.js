9f1d6721d0ee5c604058d6565ac39ea2bebcfc85
MateusGomes04 <mateusgomesc04@gmail.com>
2022-09-21 20:09:33 -0300

    accessing form information with For Each

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ var name = document.querySelector("#exampleInputName");
 var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
 var birth = document.querySelector("#exampleInputBirth");
 var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
+var email = document.querySelector("#exampleInputEmail");                                                                                      
 var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
+var photo = documen.querySelector("#exampleInputFile");
 var admin = document.querySelector("#exampleInputAdmin");
+
+
+var fields = document.querySelectorAll("form-user-create [name]");
+fields.forEach(function(field, index){
+
+    console.log(field.id, field.name, field.ariaValueMax, field.ariaChecked, index);
+
+});
