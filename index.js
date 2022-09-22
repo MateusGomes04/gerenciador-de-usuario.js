60f923f64b8e73198983112a71f87b1af9eb5399
MateusGomes04 <mateusgomesc04@gmail.com>
2022-09-21 21:14:31 -0300

    add IF and ELSE to filter the checked name gender and get the value

--- index.js
+++ index.js
@@ -2,15 +2,29 @@ var name = document.querySelector("#exampleInputName");
 var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
 var birth = document.querySelector("#exampleInputBirth");
 var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");                                                                                      
+var email = document.querySelector("#exampleInputEmail");
 var password = document.querySelector("#exampleInputPassword");
-var photo = documen.querySelector("#exampleInputFile");
+var photo = document.querySelector("#exampleInputFile");
 var admin = document.querySelector("#exampleInputAdmin");
 
 
 var fields = document.querySelectorAll("form-user-create [name]");
-fields.forEach(function(field, index){
 
-    console.log(field.id, field.name, field.ariaValueMax, field.ariaChecked, index);
+fields.forEach(function (field, index) {
+
+    if (field.name == "gender") {
+
+        if (field.checked) {
+
+            console.log("SIM", filder);
+
+        }
+
+    } else {
+
+        console.log("NÂO")
+    }
+
+    //console.log(field.id, field.name, field.ariaValueMax, field.ariaChecked, index);
 
 });
