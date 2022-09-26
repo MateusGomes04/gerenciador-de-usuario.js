8ef44e2f4ba0b6c0fa88c44087fef9681ca5a00f
MateusGomes04 <mateusgomesc04@gmail.com>
2022-09-26 12:13:04 -0300

    refactoring the code getTr method

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -47,24 +47,11 @@ class UserController {
 
                     }
 
-                    tr.dataset.user = JSON.stringify(result);
+                    let user = new User();
 
-                    tr.innerHTML = `
-                    <tr>
-                        <td><img src="${result._photo}" alt="User Image" class="img-circle img-sm"></td>
-                        <td>${result._name}</td>
-                        <td>${result._email}</td>
-                        <td>${(result._admin) ? "sim" : "não"}</td>
-                        <td>${Utils.dateFormat(result._register)}</td>
-                        <td>
-                            <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                        </td>
-                    </tr>
-                    
-                `;
+                    user.loadFromJSON(result);
 
-                    this.addEventsTr(tr);
+                    this.getTr(user, tr);
 
                     this.updateCount();
 
@@ -232,9 +219,20 @@ class UserController {
     }
 
     addLine(dataUser) {
-        let tr = document.createElement("tr");
+
+        let tr = this.getTr(dataUser);
+
+        this.tableEl.appendChild(tr);
+
+        this.updateCount();
+    }
+
+    getTr(dataUser, tr = null) {
+
+        if (tr === null) tr = document.createElement("tr");
 
         tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML = `
         <tr>
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
@@ -249,9 +247,10 @@ class UserController {
         </tr>
         
     `;
+
         this.addEventsTr(tr);
-        this.tableEl.appendChild(tr);
-        this.updateCount();
+
+        return tr;
     }
 
     addEventsTr(tr) {
